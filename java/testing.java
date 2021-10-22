9034c336997abcf70254af662530baf1044a0ee3
BouLeapheng <bouleapheng10a@gmail.com>
2021-10-22 11:52:15 +0700

    first commit

---
+++ java/testing.java
@@ -0,0 +1,8 @@
+public class testing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Rith");       
+        System.out.println("Rith plouk thom"); 
+    }
+}
---
+++ java/testing1.java
@@ -0,0 +1,8 @@
+public class testing1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Rith");       
+        System.out.println("Rith plouk thom"); 
+    }
+}
